313fc56b71bb28ce1fc1b7129de1b9bd15337b74
VenkataSanthosh <piduri.santhosh@gmail.com>
2018-02-25 23:40:17 -0600

    added transaction stuff

--- src/main/java/client/MessageClient.java
+++ src/main/java/client/MessageClient.java
@@ -3,18 +3,29 @@ package client;
 import configs.HibernateUtil;
 import entities.Message;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class MessageClient {
 
     public  static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        Transaction txt = session.beginTransaction();
+        try {
+            Message message = new Message( "Hello World with transcation stuff " );
 
-        Message message = new Message( "Hello World with Hibernate " );
+            session.save(message);
 
-        session.save(message);
+            txt.commit();
+        }catch (Exception e) {
+            if(txt!=null) {
+                txt.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
-        session.getTransaction().commit();
-        session.close();
     }
 }
